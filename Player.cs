2d795d71d5cf46c6ba41a312c9c54b7860843116
Danillo <danillo.damian@hotmail.com>
2024-07-16 20:45:55 -0300

    Add scrpit and basic movement Player

---
+++ Player.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 3.7f;
+    private float gravity = 9.81f;
+    private CharacterController characterController;
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        CalculateMovement();
+    }
+
+    private void CalculateMovement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector3 direction = new Vector3(-horizontalInput, 0, -verticalInput);
+        Vector3 velocity = direction * speed;
+        velocity.y -= gravity;
+        characterController.Move(velocity * Time.deltaTime);
+    }
+}
