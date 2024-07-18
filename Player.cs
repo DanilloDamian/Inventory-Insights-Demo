e1d85b807b9f4b3971a2e10ed5d81425a8caf873
Danillo <danillo.damian@hotmail.com>
2024-07-17 23:06:22 -0300

    Add shoot functions

--- Player.cs
+++ Player.cs
@@ -1,20 +1,78 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     [SerializeField]
     private float speed = 3.7f;
+    [SerializeField]
+    private GameObject muzzleFlash;
+    [SerializeField]
+    private GameObject hitMarketPrefab;
+    [SerializeField]
+    private AudioSource weaponAudio;
+
+    [SerializeField]
+    private int currentAmmo;
+    private int maxAmmo = 50;
+
     private float gravity = 9.81f;
     private CharacterController characterController;
+    private bool isReLoading = false;
+
+    private UIManager uiManager;
 
     void Start()
     {
         characterController = GetComponent<CharacterController>();
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        currentAmmo = maxAmmo;
+        uiManager = GameObject.Find("Canvas").GetComponent<UIManager>();
     }
 
     void Update()
     {
+        if (Input.GetMouseButton(0) && currentAmmo > 0 && !isReLoading)
+        {
+            Shoot();
+        }
+        else
+        {
+            muzzleFlash.SetActive(false);
+            weaponAudio.Stop();
+        }
+        if (Input.GetKeyDown(KeyCode.R) && !isReLoading)
+        {
+            isReLoading = true;
+            StartCoroutine(Reload());
+        }
+
         CalculateMovement();
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+    private void Shoot()
+    {
+        muzzleFlash.SetActive(true);
+        currentAmmo--;
+        uiManager.UpdateTextAmmo(currentAmmo);
+        if (!weaponAudio.isPlaying)
+        {
+            weaponAudio.Play();
+
+        }
+        Ray rayOrigin = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        RaycastHit hitInfo;
+
+        if (Physics.Raycast(rayOrigin, out hitInfo))
+        {
+            GameObject hitMarket = Instantiate(hitMarketPrefab, hitInfo.point, Quaternion.LookRotation(hitInfo.normal)) as GameObject;
+            Destroy(hitMarket, 1f);
+        }
     }
 
     private void CalculateMovement()
@@ -28,4 +86,15 @@ public class Player : MonoBehaviour
         velocity = transform.transform.TransformDirection(velocity);
         characterController.Move(velocity * Time.deltaTime);
     }
+
+    IEnumerator Reload()
+    {
+        yield return new WaitForSeconds(1.5f);
+        currentAmmo = maxAmmo;
+        uiManager.UpdateTextAmmo(currentAmmo);
+        isReLoading = false;
+    }
+
+
+
 }
---
+++ UIManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField]
+    private Text ammoText;
+
+    public void UpdateTextAmmo(int count)
+    {
+        ammoText.text = "Ammo: " + count;
+    }
+
+}
